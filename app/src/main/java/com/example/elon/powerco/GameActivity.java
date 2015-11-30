dad5ba6c82c9f7ac1a9992973ed413979a02aee7
unknown <bhay@Lindner202-16.fs.elon.edu>
2015-11-30 14:46:22 -0500

    Second commit

---
+++ app/src/main/java/com/example/elon/powerco/GameActivity.java
@@ -0,0 +1,16 @@
+package com.example.elon.powerco;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
---
+++ app/src/main/java/com/example/elon/powerco/GameLoopView.java
@@ -0,0 +1,7 @@
+package com.example.elon.powerco;
+
+/**
+ * Created by bhay on 11/30/2015.
+ */
+public class GameLoopView {
+}
